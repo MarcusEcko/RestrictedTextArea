87291b7458d827dd405379e1d7a8f1fd3ac283e0
MarcusEcko <marco237.mnm@gmail.com>
2025-07-12 13:14:00 -0300

    basic functionality added

--- src/index.js
+++ src/index.js
@@ -5,13 +5,28 @@ import './styles.css';
 class TextArea extends React.Component {
   constructor (props){
     super(props);
+    this.state = {
+      contador : 0
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const textoCompleto = event.target.value;
+    const textoCortado = textoCompleto.trim();
+
+    const cantLetras = textoCortado.length;
+
+    this.setState({contador : cantLetras});
   }
 
   render () {
     return(
       <div>
-        <h1>Wirte your message</h1>
-        <input type='text' />
+        <h1>Write your message</h1>
+        <input type='text' onChange={this.handleChange}/>
+        <h3>Cant. de Letras: {this.state.contador}</h3>
       </div>
     )
   }
