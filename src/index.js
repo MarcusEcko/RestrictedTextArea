26957aba25ecc7940f5a69ff01896648a9b0db19
MarcusEcko <marco237.mnm@gmail.com>
2025-07-12 12:41:14 -0300

    start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+
+class TextArea extends React.Component {
+  constructor (props){
+    super(props);
+  }
+
+  render () {
+    return(
+      <div>
+        <h1>Wirte your message</h1>
+        <input type='text' />
+      </div>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TextArea />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
